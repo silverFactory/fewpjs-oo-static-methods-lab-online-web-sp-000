cd0aeb69085feed9ae8558468f55584475e92196
Will Studabaker <will.studabaker@gmail.com>
2021-02-21 12:58:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.substring(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-\s-']+/g, '')
+  }
+  static titleize(string){
+    return Formatter.capitalize(string.split(" ")
+    .map(function(str){
+      if (!str.match(/^the$|^[a]$|an|but|of|and|for|at|by|from/)){
+        return Formatter.capitalize(str)
+      } else {
+        return str
+      }}).join(" "))
+  }
 }
